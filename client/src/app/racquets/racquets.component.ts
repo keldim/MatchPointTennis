b4b3dd461f44fae607f228987be8df0d2582748a
Chris Yoo <woyoo@umich.edu>
2019-08-26 04:02:48 -0400

    working on filter for racquets page

--- client/src/app/racquets/racquets.component.ts
+++ client/src/app/racquets/racquets.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./racquets.component.css']
 })
 export class RacquetsComponent implements OnInit {
+  filterExpand1: boolean = false;
+  filterExpand2: boolean = false;
+  filterExpand3: boolean = false;
+  filterExpand4: boolean = false;
+  filterExpand5: boolean = false;
+  filterExpand6: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
