c7f41a0e98d1eb307a9458441acb8e3274b44e5c
Chris Yoo <woyoo@umich.edu>
2021-06-12 04:45:00 -0400

    added tests for shipping-and-payment

--- client/src/app/shipping-and-payment/shipping-and-payment.component.spec.ts
+++ client/src/app/shipping-and-payment/shipping-and-payment.component.spec.ts
@@ -1,25 +1,150 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, fakeAsync, flush, TestBed, tick } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { BackendService } from 'src/app/services/backend.service';
+import { of } from 'rxjs';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ShippingAndPaymentComponent } from './shipping-and-payment.component';
+import { StorageService } from '../services/storage.service';
+import { By } from '@angular/platform-browser';
 
-// describe('ShippingAndPaymentComponent', () => {
-//   let component: ShippingAndPaymentComponent;
-//   let fixture: ComponentFixture<ShippingAndPaymentComponent>;
+describe('ShippingAndPaymentComponent', () => {
+  let component: ShippingAndPaymentComponent;
+  let fixture: ComponentFixture<ShippingAndPaymentComponent>;
+  let mockRouter, mockBackendService;
+  let httpTestingController: HttpTestingController;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ ShippingAndPaymentComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(() => {
+    mockRouter = jasmine.createSpyObj(['navigate']);
+    mockBackendService = jasmine.createSpyObj(['getBackendURL']);
+    TestBed.configureTestingModule({
+      declarations: [ShippingAndPaymentComponent],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientTestingModule
+      ],
+      providers: [
+        { provide: Router, useValue: mockRouter },
+        { provide: BackendService, useValue: mockBackendService }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    })
+  });
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(ShippingAndPaymentComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  it('should send request to get shipping and payment info', () => {
+    mockBackendService.getBackendURL.and.returnValue("http://localhost:5000/");
+    httpTestingController = TestBed.get(HttpTestingController);
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    fixture = TestBed.createComponent(ShippingAndPaymentComponent);
+    component = fixture.componentInstance;
+    component.ngOnInit();
+
+    const request = httpTestingController.expectOne(
+      {
+        method: 'GET',
+        url: "http://localhost:5000/form-input/ephemeral-data"
+      }
+    );
+    request.flush({});
+    // httpTestingController.verify();
+  });
+
+  it('should send shipping and payment info when the \'Continue\' button is clicked', () => {
+    mockBackendService.getBackendURL.and.returnValue("http://localhost:5000/");
+    httpTestingController = TestBed.get(HttpTestingController);
+
+    fixture = TestBed.createComponent(ShippingAndPaymentComponent);
+    component = fixture.componentInstance;
+    component.shoppingAndPaymentInfo = {};
+    component.shippingAndPaymentForm = new FormBuilder().group({
+      firstName: "josh",
+      lastName: "lee",
+      email: "random@email.com",
+      phoneNumber: "1111111111",
+
+      address1: "random address",
+      address2: "random address",
+      city: "Ann Arbor",
+      state: "MI",
+      zipcode: "10456",
+
+      cardNumber: "0000000000000000",
+      expGroup: new FormBuilder().group({
+        expMonth: "12",
+        expYear: "2025"
+      }),
+      cvc: "301"
+    });
+    fixture.detectChanges();
+    fixture.debugElement.query(By.css(".right-button")).triggerEventHandler('click', {});
+
+    const request = httpTestingController.expectOne(
+      {
+        method: 'POST',
+        url: "http://localhost:5000/form-input/data"
+      }
+    );
+    request.flush({});
+    // httpTestingController.verify();
+  });
+
+  it('should send request for clean up when the \'window:beforeunload\' event is triggered', () => {
+    mockBackendService.getBackendURL.and.returnValue("http://localhost:5000/");
+    httpTestingController = TestBed.get(HttpTestingController);
+
+    fixture = TestBed.createComponent(ShippingAndPaymentComponent);
+    component = fixture.componentInstance;
+    window.dispatchEvent(new Event('beforeunload'));
+
+    const request = httpTestingController.expectOne(
+      {
+        method: 'POST',
+        url: "http://localhost:5000/form-input/clean-up"
+      }
+    );
+    request.flush({});
+    // httpTestingController.verify();
+  });
+
+  it('should send cancel request when the \'Cancel\' button is clicked', () => {
+    mockBackendService.getBackendURL.and.returnValue("http://localhost:5000/");
+    httpTestingController = TestBed.get(HttpTestingController);
+
+    fixture = TestBed.createComponent(ShippingAndPaymentComponent);
+    component = fixture.componentInstance;
+    component.shoppingAndPaymentInfo = {};
+    component.shippingAndPaymentForm = new FormBuilder().group({
+      firstName: "josh",
+      lastName: "lee",
+      email: "random@email.com",
+      phoneNumber: "1111111111",
+
+      address1: "random address",
+      address2: "random address",
+      city: "Ann Arbor",
+      state: "MI",
+      zipcode: "10456",
+
+      cardNumber: "0000000000000000",
+      expGroup: new FormBuilder().group({
+        expMonth: "12",
+        expYear: "2025"
+      }),
+      cvc: "301"
+    });
+    fixture.detectChanges();
+    fixture.debugElement.query(By.css(".left-button")).triggerEventHandler('click', {});
+
+    const request = httpTestingController.expectOne(
+      {
+        method: 'POST',
+        url: "http://localhost:5000/form-input/cancel"
+      }
+    );
+    request.flush({});
+    // httpTestingController.verify();
+  });
+});
